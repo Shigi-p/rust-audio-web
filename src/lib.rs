e487e218ae62f9e920ba5591169c7aefb2ea4867
4D-Daisuke-Kurasaki <daisuke.kurasaki@4digit.jp>
2023-12-15 14:25:57 +0900

    Revert ":tada: first-commit"
    This reverts commit f5047706a34fa36669cfa069ee7d82267d6c8ffb.

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-use wasm_bindgen::prelude::*;
-use std::f64::consts::PI;
-
-#[wasm_bindgen]
-pub fn sine(current_frame: f64, sample_rate: f64, i: f64) -> f64 {
-    0.5 * ((440.0 * 2.0 * PI * (current_frame + i)) / sample_rate).sin()
-}
